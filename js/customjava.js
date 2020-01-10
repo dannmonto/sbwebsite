de3eadd6d9970151f9361ac5eba75f0d38807cee
dannmonto <58283037+dannmonto@users.noreply.github.com>
2020-01-10 16:16:51 -0500

    nav fade

--- js/customjava.js
+++ js/customjava.js
@@ -9,14 +9,25 @@
 //   prevScrollpos = currentScrollPos;
 // } 
 
+
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-80px";
-  }
-  prevScrollpos = currentScrollPos;
-} 
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("nav").style.top = "0";
+//   } else {
+//     document.getElementById("nav").style.top = "-80px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// } 
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.nav').fadeOut();
+    } else {
+      $('.nav').fadeIn();
+    }
+  });
+
