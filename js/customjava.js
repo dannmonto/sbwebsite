85451627f0f34965e1b60a131f40c761e922b0fe
dannmonto <58283037+dannmonto@users.noreply.github.com>
2020-01-26 15:40:36 -0500

    changes to slider

--- js/customjava.js
+++ js/customjava.js
@@ -1,5 +1,6 @@
 
 // Custom options for FancyBox 
+
 $.fancybox.defaults.infobar = true; // Displays image count top left
 $.fancybox.defaults.toolbar = true; // Displays toolbar (thumbnails, play slideshow..)
 // Choose which buttons for toolbar
@@ -14,3 +15,13 @@ $.fancybox.defaults.buttons =  [
   ];
 // Simple protect images (right click)
   $.fancybox.defaults.protect = true;
+
+  // No zoom on tap
+  $.fancybox.defaults.clickContent = false;
+
+  // On mobile, click outside to close slide
+  $.fancybox.defaults.mobile = {
+    clickSlide: 'close',
+    clickOutside: 'close',
+    clickContent: false
+  }
